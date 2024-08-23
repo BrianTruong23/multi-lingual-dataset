88c025da4d32efbc122bef23dab4bc3c3a051788
briantruong23 <truongthoithang@gmail.com>
2024-08-23 10:30:41 -0400

    add file path

--- read_in_file.py
+++ read_in_file.py
@@ -1,7 +1,7 @@
 import pandas as pd
 import pyarrow.parquet as pq
 
-file_name = "data-00000-of-00001.arrow"
+file_name = "vie/MMMU/Accounting/devdata-00000-of-00001.arrow"
 
 # Read the Arrow file into a Pandas DataFrame
 table = pq.read_table(file_name)
